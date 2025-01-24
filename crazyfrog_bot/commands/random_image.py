a24ed8d93eb864cfe0f1414ea9828f8998e1c026
TheDwahgon <therealdragonofwar@gmail.com>
2025-01-23 21:42:52 -0300

    feat: support file images

--- crazyfrog_bot/commands/random_image.py
+++ crazyfrog_bot/commands/random_image.py
@@ -1,6 +1,9 @@
 import discord
 import logging
 import random
+import re
+from utils.regex import URL_REGEX
+
 
 def setup(tree: discord.app_commands.CommandTree, name: str, description: str, logger: logging.Logger, extra_args = {}):
     @tree.command(name=name, description=description)
@@ -10,10 +13,13 @@ def setup(tree: discord.app_commands.CommandTree, name: str, description: str, l
         texts = extra_args['texts']
 
         image = images[random.randint(0, len(images))]
-        text = texts[random.randint(0, len(images))]
+        text = texts[random.randint(0, len(texts))]
         logger.debug(f'Chosen image: {image}')
         logger.debug(f'Chosen text: {text}')
 
-        embed = discord.Embed()
-        embed.set_image(url=image)
-        await interation.response.send_message(text, embed=embed)
+        if re.match(URL_REGEX, image):
+            embed = discord.Embed()
+            embed.set_image(url=image)
+            await interation.response.send_message(text, embed=embed)
+        else:
+            await interation.response.send_message(text, file=discord.File(image))
